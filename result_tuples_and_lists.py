d11070c5aef21e210da7832b19c6061cff432eac
Wilhelm <wilvo970@student.liu.se>
2022-12-18 08:49:41 +0100

    assignment 4 done

--- result_tuples_and_lists.py
+++ result_tuples_and_lists.py
@@ -1,28 +1,6 @@
 #!/usr/bin/python3.9
 #-*- coding: utf-8; mode: python; -*-
 
-# För att köra senaste variant av python:
-#       1. sudo apt update
-#       2. sudo apt install python3.10
-#       3. ls /usr/bin | grep python    # To check what versions are available
-
-# def modification_test(seq1: list, seq2: list, i: int) -> None:
-#     seq1 = [1, 2, 3]                                    # seq1 objektet uppdateras ej
-#     seq2.append("From inside modifictaion_test")        # seq2 objektet uppdateras
-#     i = 39                                              # i objektet uppdateras ej
-
-# def main():
-#     number_list = [1,2]
-#     other_list = ["Hello, 15, 6.23"]
-#     number = 9
-#     modification_test(number_list, other_list, number)
-#     print(number_list)
-#     print(other_list)
-#     print(number)
-
-
-# main()
-
 def already_exists(item  : tuple,
                    item_list : list) -> bool:
 
